7e6a081e8d5fdf01a3b6c0f7260ebcaa82453404
SmooKboi <smooksmurf@hotmail.com>
2019-02-23 05:43:43 +0100

    Added Auto Stream Name Detection and some other stuff

--- TwitchStocks Helper/Form1.Designer.cs
+++ TwitchStocks Helper/Form1.Designer.cs
@@ -28,16 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.metroButton1 = new MetroFramework.Controls.MetroButton();
             this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
             this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroComboBox2 = new MetroFramework.Controls.MetroComboBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.changeName = new System.Windows.Forms.Timer(this.components);
+            this.metroCheckBox1 = new MetroFramework.Controls.MetroCheckBox();
+            this.metroToolTip1 = new MetroFramework.Components.MetroToolTip();
             this.SuspendLayout();
             // 
             // metroButton1
             // 
             this.metroButton1.Location = new System.Drawing.Point(163, 242);
             this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(117, 36);
+            this.metroButton1.Size = new System.Drawing.Size(117, 29);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Orange;
             this.metroButton1.TabIndex = 0;
             this.metroButton1.Text = "Copy to Clipboard";
             this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -60,7 +68,6 @@
             this.metroComboBox1.TabIndex = 1;
             this.metroComboBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.metroComboBox1.UseSelectable = true;
-            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.selectMessageIndexChanged);
             // 
             // metroTextBox1
             // 
@@ -94,21 +101,96 @@
             this.metroTextBox1.WaterMark = "Streamer Name";
             this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
             this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
-            this.metroTextBox1.TextChanged += new System.EventHandler(this.streamerNameTextChanged);
+            this.metroTextBox1.TextChanged += new System.EventHandler(this.metroTextBox1_TextChanged);
+            // 
+            // metroComboBox2
+            // 
+            this.metroComboBox2.FormattingEnabled = true;
+            this.metroComboBox2.ItemHeight = 23;
+            this.metroComboBox2.Items.AddRange(new object[] {
+            "x1",
+            "x4",
+            "x8",
+            "x12",
+            "x16",
+            "x20"});
+            this.metroComboBox2.Location = new System.Drawing.Point(23, 242);
+            this.metroComboBox2.Name = "metroComboBox2";
+            this.metroComboBox2.Size = new System.Drawing.Size(47, 29);
+            this.metroComboBox2.TabIndex = 3;
+            this.metroComboBox2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroComboBox2.UseSelectable = true;
+            this.metroComboBox2.SelectedIndexChanged += new System.EventHandler(this.selectAmountChanged);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(18, 220);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(101, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "Post Message ...";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(18, 274);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(53, 19);
+            this.metroLabel2.TabIndex = 5;
+            this.metroLabel2.Text = "... times";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // changeName
+            // 
+            this.changeName.Enabled = true;
+            this.changeName.Interval = 1000;
+            this.changeName.Tick += new System.EventHandler(this.changeName_Tick);
+            // 
+            // metroCheckBox1
+            // 
+            this.metroCheckBox1.AutoSize = true;
+            this.metroCheckBox1.Location = new System.Drawing.Point(23, 167);
+            this.metroCheckBox1.Name = "metroCheckBox1";
+            this.metroCheckBox1.Size = new System.Drawing.Size(144, 15);
+            this.metroCheckBox1.TabIndex = 6;
+            this.metroCheckBox1.Text = "Disable Auto Detection";
+            this.metroCheckBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroCheckBox1.UseSelectable = true;
+            this.metroCheckBox1.CheckedChanged += new System.EventHandler(this.metroCheckBox1_CheckedChanged);
+            this.metroCheckBox1.MouseHover += new System.EventHandler(this.metroCheckBox1_MouseHover);
+            // 
+            // metroToolTip1
+            // 
+            this.metroToolTip1.AutoPopDelay = 0;
+            this.metroToolTip1.InitialDelay = 500;
+            this.metroToolTip1.ReshowDelay = 100;
+            this.metroToolTip1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroToolTip1.StyleManager = null;
+            this.metroToolTip1.Theme = MetroFramework.MetroThemeStyle.Light;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(303, 301);
+            this.Controls.Add(this.metroCheckBox1);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroComboBox2);
             this.Controls.Add(this.metroTextBox1);
             this.Controls.Add(this.metroComboBox1);
             this.Controls.Add(this.metroButton1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.Resizable = false;
             this.Style = MetroFramework.MetroColorStyle.Orange;
             this.Text = "TwitchStocks.com Helper";
             this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -117,6 +199,12 @@
         private MetroFramework.Controls.MetroButton metroButton1;
         private MetroFramework.Controls.MetroComboBox metroComboBox1;
         private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        public System.Windows.Forms.Timer changeName;
+        private MetroFramework.Controls.MetroCheckBox metroCheckBox1;
+        public MetroFramework.Components.MetroToolTip metroToolTip1;
     }
 }
 
--- TwitchStocks Helper/Form1.cs
+++ TwitchStocks Helper/Form1.cs
@@ -3,11 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Automation;
+using System.Net;
+using System.IO;
 
 namespace TwitchStocks_Helper
 {
@@ -15,44 +19,131 @@ namespace TwitchStocks_Helper
     {
         public Form1()
         {
-            this.Icon = new Icon("Resources/baseline-bar_chart-24px.ico");
             InitializeComponent();
+            metroComboBox1.SelectedIndex = 0;
+            metroComboBox2.SelectedIndex = 0;
         }
 
-        private string StreamerName;
-        private string Message;
+        private string StreamerName = "";
+        private string Message = "null";
+        private string Amount;
+        private int AmountTimes;
+        private bool isTimerDisabled = false;
 
-        private void selectMessageIndexChanged(object sender, EventArgs e)
+        private string[] getStreamerNames()
         {
-            Message = metroComboBox1.SelectedItem.ToString();
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process process in processlist)
+            {
+                if (process.MainWindowTitle.StartsWith("TwitchStocks - "))
+                {
+                    string processName = process.MainWindowTitle.Replace("TwitchStocks - ", "").Replace("Google Chrome", "");
+                    string[] names = processName.Split(' ');
+                    return names;
+                }
+            }
+            return null;
         }
 
-        private void streamerNameTextChanged(object sender, EventArgs e)
+        private void selectAmountChanged(object sender, EventArgs e)
         {
-            StreamerName = metroTextBox1.Text;
+            Amount = metroComboBox2.SelectedItem.ToString();
+            switch (Amount)
+            {
+                case "x1":
+                    AmountTimes = 1;
+                    break;
+                case "x4":
+                    AmountTimes = 4;
+                    break;
+                case "x8":
+                    AmountTimes = 8;
+                    break;
+                case "x12":
+                    AmountTimes = 12;
+                    break;
+                case "x16":
+                    AmountTimes = 16;
+                    break;
+                case "x20":
+                    AmountTimes = 20;
+                    break;
+            }
         }
 
         private void copyClick(object sender, EventArgs e)
         {
-            switch (Message)
+            StreamerName = metroTextBox1.Text;
+            switch (metroComboBox1.SelectedItem.ToString())
             {
                 case "Buy":
-                    Message = ":chart_with_upwards_trend: BUY " + StreamerName + " :chart_with_upwards_trend:";
+                    Message = ":chart_with_upwards_trend: BUY **" + StreamerName + "** :chart_with_upwards_trend: \n";
                     break;
                 case "Sell":
-                    Message = ":chart_with_downwards_trend: SELL " + StreamerName + " :chart_with_downwards_trend:";
+                    Message = ":chart_with_downwards_trend: SELL **" + StreamerName + "** :chart_with_downwards_trend: \n";
                     break;
                 case "Hold":
-                    Message = ":raised_hand: " + StreamerName + " :raised_hand:";
+                    Message = ":raised_hand: HOLD **" + StreamerName + "** :raised_hand: \n";
                     break;
                 case "Warn":
-                    Message = ":warning: " + StreamerName + " MIGHT BE RISKY; TRY 1000 SHARES :warning:";
+                    Message = ":warning: **" + StreamerName + "** MIGHT BE RISKY; TRY 1000 SHARES :warning: \n";
                     break;
             }
+            Clipboard.SetText(String.Concat(Enumerable.Repeat(Message, AmountTimes)).ToString());
+        }
 
-            if (Message != null)
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void changeName_Tick(object sender, EventArgs e)
+        {
+            if (getStreamerNames() != null)
             {
-                Clipboard.SetText(Message);
+                Console.WriteLine(getStreamerNames()[0]);
+                if ((getStreamerNames()[0] != "Home") && (getStreamerNames()[0] != "Leaderboards") && (getStreamerNames()[0] != "History") && (getStreamerNames()[0] != "Portfolio") & (getStreamerNames()[0] != "FAQ") & (getStreamerNames()[0] != "Legal"))
+                {
+                    metroTextBox1.Text = getStreamerNames()[0] + " (" + getStreamerNames()[1].ToUpper() + ")";
+                }
+            }
+        }
+
+        private void metroTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (metroTextBox1.Text == "")
+            {
+                if (!isTimerDisabled)
+                {
+                    changeName.Start();
+                }
+            }
+            else
+            {
+                changeName.Stop();
+            }
+        }
+
+        private void metroCheckBox1_MouseHover(object sender, EventArgs e)
+        {
+
+            metroToolTip1.Show("Disables Auto Detection of the Streamer Name \n" +
+                "Check this if you encounter bugs", metroCheckBox1);
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (metroCheckBox1.Checked)
+            {
+                changeName.Stop();
+                isTimerDisabled = true;
+            }
+            else
+            {
+                changeName.Start();
+                isTimerDisabled = false;
             }
         }
     }
